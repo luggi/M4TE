21a5f6b03b233d92db3f1b19106347daa42945a2
Franz-Josef Haider <f_haider@gmx.at>
2014-11-07 09:30:01 +0100

    include iostream in log.h

--- src/log.h
+++ src/log.h
@@ -1,6 +1,8 @@
 #ifndef __LOG_H__
 #define __LOG_H__
 
+#include <iostream>
+
 #define LOG_TAG "IRCBot: "
 #define LOG_ERROR(msg) std::cerr << LOG_TAG "error: " << msg << std::endl;
 #define LOG_INFO(msg) std::cout << LOG_TAG "info: " << msg << std::endl;
