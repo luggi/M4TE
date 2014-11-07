12a9804176a96b61a4c00f3574e19b43ae3f6f12
Franz-Josef Haider <f_haider@gmx.at>
2014-11-07 09:15:28 +0100

    fix LOG_ERROR and add LOG_INFO

--- src/log.h
+++ src/log.h
@@ -2,7 +2,8 @@
 #define __LOG_H__
 
 #define LOG_TAG "IRCBot: "
-#define LOG_ERROR(msg) std::cerr << LOG_TAG "error: " msg << std::endl;
+#define LOG_ERROR(msg) std::cerr << LOG_TAG "error: " << msg << std::endl;
+#define LOG_INFO(msg) std::cout << LOG_TAG "info: " << msg << std::endl;
 
 #endif /* __LOG_H__ */
 
