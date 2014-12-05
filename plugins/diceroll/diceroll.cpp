993cf42e2e9b9aeb40f218fe316d11faebdcce67
Alex Hirsch <w4rh4wk@bluephoenix.at>
2014-12-05 08:53:37 +0100

    call srand only in construcotr

--- plugins/diceroll/diceroll.cpp
+++ plugins/diceroll/diceroll.cpp
@@ -8,9 +8,13 @@ using namespace std;
 
 class diceRoll : public plugin {
     public:
-        string call(const string command) const
+        diceRoll()
         {
             srand (unsigned(time(NULL)));
+        }
+
+        string call(const string command) const
+        {
             int randomNumber = rand() % 5 + 1;
             string str = to_string(randomNumber);
             return str;
