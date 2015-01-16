4fa08d155cca8b116f0d42bc4793c6654449c065
Alex Hirsch <w4rh4wk@bluephoenix.at>
2015-01-16 09:14:46 +0100

    move template implementation to header file to fix unknown symbols

--- plugins/pushpop/stack.h
+++ plugins/pushpop/stack.h
@@ -26,4 +26,34 @@ class Stack {
         void clear();
 };
 
+template<class T>
+void Stack<T>::push(const T &e)
+{
+    elems.push_back(e);
+}
+
+template<class T>
+T Stack<T>::pop()
+{
+    if (elems.empty()) {
+        throw out_of_range("stack empty");
+    }
+
+    T ret = elems.back();
+    elems.pop_back();
+    return ret;
+}
+
+template<class T>
+bool Stack<T>::empty() const
+{
+    return elems.empty();
+}
+
+template<class T>
+void Stack<T>::clear()
+{
+    elems.clear();
+}
+
 #endif
